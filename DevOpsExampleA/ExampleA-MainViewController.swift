9bf943d76d54f160fb2f6588a2ff2069348fda3b
Parker McLain <>
2020-02-20 10:54:30 -0500

    add some views for debugging

---
+++ DevOpsExampleA/ExampleA-MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewController.swift
+//  DevOpsExampleA
+//
+//  Created by Parker McLain on 2/20/20.
+//  Copyright © 2020 Parker McLain. All rights reserved.
+//
+
+import UIKit
+
+class ExampleAMainViewController: UIViewController {
+    @IBOutlet var key1Label: UILabel!
+    @IBOutlet var buildNumberLabel: UILabel!
+    @IBOutlet var versionNumberLabel: UILabel!
+    @IBOutlet var configurationLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        if let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+           buildNumberLabel.text = "Build: \(build)"
+        }
+        
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+           versionNumberLabel.text = "Version: \(version)"
+        }
+        
+        key1Label.text = "Key1: \(APIKeys.key1)"
+        configurationLabel.text = configuration
+    }
+    
+    let configuration : String = {
+        #if DEBUG
+            return "DEBUG"
+        #elseif RELEASE
+            return "RELEASE"
+        #elseif APPSTORE
+            return "APPSTORE"
+        #else
+            return "Unknown"
+        #endif
+    }()
+
+}
